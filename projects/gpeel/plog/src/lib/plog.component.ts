49d479d5a8613c1a552b8f4440a49b9ec1d96635
Gauthier PEEL <gauthier.peel@gmail.com>
2023-07-13 16:38:11 +0200

    installed eslint + lib project

---
+++ projects/gpeel/plog/src/lib/plog.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-plog',
+  template: `
+    <p>
+      plog works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PlogComponent {
+
+}
